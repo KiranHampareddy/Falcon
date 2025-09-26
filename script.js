83e222a111dfbee71c623744d2d1c139162d116a
KiranHampareddy <hkiran195@gmail.com>
2025-09-26 03:44:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -153,8 +153,8 @@
     }
 
     function showPlaceholder() {
-        outputPanel.innerHTML = `<div class="output-wrapper"><div class="output-placeholder"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" /></svg><h2>Your Results Will Appear Here</h2><p>Select a tool, upload your file(s), and click process.</p></div></div>`;
-    }
+    outputPanel.innerHTML = `<div class="output-wrapper"><div class="output-placeholder"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" /></svg><h2>Your Results Will Appear Here</h2><p>Select a tool, upload your file(s), and click process.</p></div></div>`;
+}
 
     function showLoading() {
         outputPanel.innerHTML = `<div class="output-wrapper"><div class="loading-spinner"></div></div>`;
@@ -873,3 +873,4 @@
         });
     }
 })();
+
